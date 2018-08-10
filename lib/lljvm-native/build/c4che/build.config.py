cf48bb9c0ac050cf3dbb55823221b0ca7b40d400
EC2 Default User <ec2-user@ip-172-31-21-224.us-west-2.compute.internal>
2018-08-10 10:45:37 +0000

    Update .gitignore

--- lib/lljvm-native/build/c4che/build.config.py
+++
@@ -1,2 +0,0 @@
-version = 0x2000200
-tools = [{'tool': 'ar', 'tooldir': None, 'funs': None}, {'tool': 'cxx', 'tooldir': None, 'funs': None}, {'tool': 'clang++', 'tooldir': None, 'funs': None}, {'tool': 'compiler_cxx', 'tooldir': None, 'funs': None}, {'tool': 'waf_unit_test', 'tooldir': None, 'funs': None}]
