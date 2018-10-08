441985dad9db9b630370c769f49b218feeca8f2b
Takeshi Yamamuro <yamamuro@apache.org>
2018-10-08 20:06:59 +0900

    Fix style issues

--- core/src/main/java/io/github/maropu/lljvm/util/Pair.java
+++ core/src/main/java/io/github/maropu/lljvm/util/Pair.java
@@ -18,19 +18,19 @@
 package io.github.maropu.lljvm.util;
 
 public class Pair<K, V> {
-    private K key;
-    private V value;
+  private K key;
+  private V value;
 
-    public Pair(K key, V value) {
-      this.key = key;
-      this.value = value;
-    }
+  public Pair(K key, V value) {
+    this.key = key;
+    this.value = value;
+  }
 
-    public K getKey() {
-      return key;
-    }
+  public K getKey() {
+    return key;
+  }
 
-    public V getValue() {
-      return value;
-    }
+  public V getValue() {
+    return value;
+  }
 }
