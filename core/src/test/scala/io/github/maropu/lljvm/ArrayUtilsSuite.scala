90809eeb1ceea3d8a3b05025d8d9ee93c44ac719
Takeshi Yamamuro <yamamuro@apache.org>
2018-11-20 14:27:50 +0900

    Brush up test code

--- core/src/test/scala/io/github/maropu/lljvm/ArrayUtilsSuite.scala
+++ core/src/test/scala/io/github/maropu/lljvm/ArrayUtilsSuite.scala
@@ -25,7 +25,7 @@ class ArrayUtilsSuite extends LLJVMFuncSuite {
   test("boolean") {
     val booleanArray = Array(true, false, true, true, false, false, true)
     val baseAddr = ArrayUtils.addressOf(booleanArray)
-    (0 until booleanArray.size).foreach { i =>
+    booleanArray.indices.foreach { i =>
       assert(booleanArray(i) === Platform.getBoolean(null, baseAddr + i))
     }
   }
@@ -33,7 +33,7 @@ class ArrayUtilsSuite extends LLJVMFuncSuite {
   test("byte") {
     val byteArray = Array(1, 3, 8, 3, 2, 5, 2, 8, 1, 1, 4).map(_.toByte)
     val baseAddr = ArrayUtils.addressOf(byteArray)
-    (0 until byteArray.size).foreach { i =>
+    byteArray.indices.foreach { i =>
       assert(byteArray(i) === Platform.getByte(null, baseAddr + i))
     }
   }
@@ -41,7 +41,7 @@ class ArrayUtilsSuite extends LLJVMFuncSuite {
   test("short") {
     val shortArray = Array(8, 9, 3, 5, 4, 8, 2, 3, 5, 6, 9).map(_.toShort)
     val baseAddr = ArrayUtils.addressOf(shortArray)
-    (0 until shortArray.size).foreach { i =>
+    shortArray.indices.foreach { i =>
       assert(shortArray(i) === Platform.getShort(null, baseAddr + 2 * i))
     }
   }
@@ -49,7 +49,7 @@ class ArrayUtilsSuite extends LLJVMFuncSuite {
   test("int") {
     val intArray = Array(1, 1, 8, 3, 3, 4, 1, 0, 7, 3, 4, 3)
     val baseAddr = ArrayUtils.addressOf(intArray)
-    (0 until intArray.size).foreach { i =>
+    intArray.indices.foreach { i =>
       assert(intArray(i) === Platform.getInt(null, baseAddr + 4 * i))
     }
   }
@@ -57,7 +57,7 @@ class ArrayUtilsSuite extends LLJVMFuncSuite {
   test("long") {
     val longArray = Array(0, 0, 5, 8, 1, 9, 2, 3, 8, 7, 1, 2).map(_.toLong)
     val baseAddr = ArrayUtils.addressOf(longArray)
-    (0 until longArray.size).foreach { i =>
+    longArray.indices.foreach { i =>
       assert(longArray(i) === Platform.getInt(null, baseAddr + 8 * i))
     }
   }
@@ -65,7 +65,7 @@ class ArrayUtilsSuite extends LLJVMFuncSuite {
   test("float") {
     val floatArray = Array(5, 8, 3, 2, 8, 3, 6, 6, 1, 8, 2, 5).map(_.toFloat)
     val baseAddr = ArrayUtils.addressOf(floatArray)
-    (0 until floatArray.size).foreach { i =>
+    floatArray.indices.foreach { i =>
       assert(floatArray(i) === Platform.getFloat(null, baseAddr + 4 * i))
     }
   }
@@ -73,7 +73,7 @@ class ArrayUtilsSuite extends LLJVMFuncSuite {
   test("double") {
     val doubleArray = Array(9, 8, 1, 0, 5, 3, 4, 4, 1, 8, 3, 1).map(_.toDouble)
     val baseAddr = ArrayUtils.addressOf(doubleArray)
-    (0 until doubleArray.size).foreach { i =>
+    doubleArray.indices.foreach { i =>
       assert(doubleArray(i) === Platform.getDouble(null, baseAddr + 8 * i))
     }
   }
